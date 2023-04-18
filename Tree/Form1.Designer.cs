db9954a87738893646b773f5e5b1aa1f3a978c09
mariam <mariammuza@mail.ru>
2023-04-18 08:54:57 +0500

    commit =)

---
+++ Tree/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonGenateTable = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.buttonSaveToFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(370, 357);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(401, 53);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(182, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(401, 93);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(182, 23);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Сохранить изменения";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(401, 134);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(182, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(401, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonGenateTable
+            // 
+            this.buttonGenateTable.Location = new System.Drawing.Point(401, 175);
+            this.buttonGenateTable.Name = "buttonGenateTable";
+            this.buttonGenateTable.Size = new System.Drawing.Size(182, 23);
+            this.buttonGenateTable.TabIndex = 6;
+            this.buttonGenateTable.Text = "Сгенерировать отчет";
+            this.buttonGenateTable.UseVisualStyleBackColor = true;
+            this.buttonGenateTable.Click += new System.EventHandler(this.buttonGenateTable_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.webBrowser1.Location = new System.Drawing.Point(607, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(434, 357);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // buttonSaveToFile
+            // 
+            this.buttonSaveToFile.Location = new System.Drawing.Point(401, 234);
+            this.buttonSaveToFile.Name = "buttonSaveToFile";
+            this.buttonSaveToFile.Size = new System.Drawing.Size(182, 23);
+            this.buttonSaveToFile.TabIndex = 8;
+            this.buttonSaveToFile.Text = "Сохранить отчет в файл";
+            this.buttonSaveToFile.UseVisualStyleBackColor = true;
+            this.buttonSaveToFile.Click += new System.EventHandler(this.buttonSaveToFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(406, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "(Двойной клик)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 357);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSaveToFile);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.buttonGenateTable);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Создание дерева и генерация отчета в HTML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonGenateTable;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button buttonSaveToFile;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tree/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tree
+{
+    public partial class Form1 : Form
+    {
+        string htmlStartCode;
+        string htmlCode;
+        string htmlEndCode;
+        StreamWriter sw = new StreamWriter("table.html");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode(textBox1.Text);
+            try
+            {
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+            catch 
+            { 
+            treeView1.Nodes.Add(node);
+            }
+            
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+             treeView1.SelectedNode.Text=textBox1.Text ;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox1.Text = treeView1.SelectedNode.Text;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+
+        private void buttonGenateTable_Click(object sender, EventArgs e)
+        {
+            //обнуляем
+            webBrowser1.DocumentText = null;
+            htmlCode = null;
+            //выводим начало
+            htmlStartCode = "<!DOCTYPE HTML><html> <head>  <meta charset='utf-8'>  "
+                + "<title>Отчет =) </title> "
+                + "<style type='text/css'>"
+                + " table { border-collapse: collapse;} "
+                + " tr,th {border: 1px solid black;}  </style>"
+                + "</head> <body> <table>";
+            //вызываем рекурсивный метод
+            foreach (TreeNode n in treeView1.Nodes)
+            {
+                PrintRecursive(n);
+            }    
+            //вывод конца
+            htmlEndCode = "</table> </body> </html>";           
+        }
+        private void PrintRecursive(TreeNode treeNode)
+        {
+            //проверка уровня узла
+            switch (treeNode.Level)
+            {
+                case 0: htmlCode += "<tr> <th bgcolor=#CCCCCC>" + treeNode.Text + "</th> <th></th> </tr>"; break;
+                case 1: htmlCode += "<tr> <th bgcolor=#CCCCCC>" + treeNode.Text + "</th> <th></th> </tr>"; break;
+                case 2: htmlCode += "<tr> <th ></th> <th>" + treeNode.Text + "</th> </tr>"; break;
+            }                
+            //отображение всех частей html кода
+            webBrowser1.DocumentText = htmlStartCode + htmlCode + htmlEndCode;
+
+            // рекурсия для каждого узла
+            foreach (TreeNode tn in treeNode.Nodes)
+            {   
+                PrintRecursive(tn);
+            }
+            
+        }
+
+        private void buttonSaveToFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sw.WriteLine(webBrowser1.DocumentText);
+                sw.Close();
+                MessageBox.Show("Отчет сохранен в файл table.html =)");
+            }
+            catch 
+            {
+                MessageBox.Show("Отчет не сохранен в файл =(");
+            }
+        }
+          
+    }
+}
